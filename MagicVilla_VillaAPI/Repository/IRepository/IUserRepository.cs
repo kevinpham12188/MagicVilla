10412874484c8785976c4d9e4e86ea3de1154196
kevinpham12188 <kevinpham7788@gmail.com>
2025-04-21 13:16:27 -0700

    Add User Repository

---
+++ MagicVilla_VillaAPI/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        bool IsUniqeUser(string userName);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO);
+    }
+}
---
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Repository.IRepository;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool IsUniqeUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
